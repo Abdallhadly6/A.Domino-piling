b97d32d8a27dc336bc1aa1d066f46f745420f343
abdallh <abdallhadly6@gmail.com>
2022-07-06 06:36:37 +0200

    first

---
+++ src/a/domino/piling/ADominoPiling.java
@@ -0,0 +1,16 @@
+
+package a.domino.piling;
+
+import java.util.Scanner;
+
+public class ADominoPiling {
+
+    public static Scanner inp = new Scanner(System.in);
+    public static void main(String[] args) {
+        int m , n;
+        m = inp.nextInt();
+        n = inp.nextInt();
+        System.out.println((m*n)/2);
+    }
+    
+}
